d7d3cb73374557a3bcb482d4cac17f943f1683e1
Tomoko Jinno <tomokojinno@macbook.local>
2024-08-20 15:17:00 +0900

    update

--- js/main.js
+++ js/main.js
@@ -17,6 +17,10 @@
         $('#tab_all').on('click',function(){
             $('.regular').fadeIn(fadeIn_time);
             $('.seasonal').fadeIn(fadeIn_time);
+            // $('.regular').addClass('active');
+            // $('.seasonal').addClass('active');
+            $('.flavor_cards').slick('slickUnfilter');
+
 
             $('#tab_all').addClass('active');
             $('#tab_regular').removeClass('active');
@@ -26,6 +30,10 @@
         $('#tab_regular').on('click',function(){
             $('.regular').fadeIn(fadeIn_time);
             $('.seasonal').fadeOut(fadeOut_time);
+            // $('.regular').addClass('active');
+            // $('.seasonal').removeClass('active');
+            $('.flavor_cards').slick('slickUnfilter');
+            $('.flavor_cards').slick('slickFilter','.regular');
 
             $('#tab_all').removeClass('active');
             $('#tab_regular').addClass('active');
@@ -34,34 +42,102 @@
         $('#tab_seasonal').on('click',function(){
             $('.regular').fadeOut(fadeOut_time);
             $('.seasonal').fadeIn(fadeIn_time);
+            // $('.regular').removeClass('active');
+            // $('.seasonal').addClass('active');
+            $('.flavor_cards').slick('slickUnfilter');
+            $('.flavor_cards').slick('slickFilter','.seasonal');
 
             $('#tab_all').removeClass('active');
             $('#tab_regular').removeClass('active');
             $('#tab_seasonal').addClass('active');
         });    
     });
-
+ 
     $(function(){
         $('.flavor_cards').slick({
             arrows: true, // 前・次のボタンを表示する
             dots: true, // ドットナビゲーションを表示する
+            speed: 1000, // スライドさせるスピード（ミリ秒）
             appendArrows: $('.slider'),
             appendDots: $('.slider'), // ドットナビゲーションの生成位置を変更
-            speed: 1000, // スライドさせるスピード（ミリ秒）
             slidesToShow: 1, // 表示させるスライド数
             centerMode: true, // slidesToShowが奇数のとき、現在のスライドを中央に表示する
             variableWidth: true, // スライド幅の自動計算を無効化
-            autoplay: false, //自動再生
-            autoplaySpeed: 2000,
+            autoplay: true, //自動再生
+            autoplaySpeed: 3000,
             pauseOnHover: true,
+            pauseOnFocus: true,
+
+            pauseOnDotsHover: true,
+
             responsive: [{
                     breakpoint: 768,
                     settings: {
                         autoplay: true, //自動再生
                         slidesToShow: 1, // 表示させるスライド数
+                        appendArrows: $('.slider'),
+                        appendDots: $('.slider'), // ドットナビゲーションの生成位置を変更
                     }
                     }],
         });
+
+        const flavors = ["chocolate", "mocha", "matcha", "vanilla", "pistachio", "strawberry", "lemon", "guava", "mikan", "plum"];
+        const seasonal_flavors = ["lemon", "guava", "mikan", "plum"];
+
+        $('.flavor_label').on('click', function(){
+            var i_clicked = 0;
+            if($('#tab_seasonal').hasClass('active')) {
+                seasonal_flavors.forEach((flv, i) => {
+                    if ($(this).hasClass(flv)) {
+                        i_clicked = i;
+                        return;
+                    }
+                });
+
+            } else {
+                flavors.forEach((flv, i) => {
+                    if ($(this).hasClass(flv)) {
+                        i_clicked = i;
+                        return;
+                    }
+                });
+            }
+
+            $('.flavor_cards').slick('slickGoTo', i_clicked, false);
+            console.log(i_clicked);
+            console.log($('#tab_seasonal').hasClass('active'));
+        });
+
+        $('.flavor_cards').on('init reInit afterChange', function(event, slick, currentSlide, nextSlide){
+            //currentSlide is undefined on init -- set it to 0 in this case (currentSlide is 0 based)
+            // console.log(currentSlide);
+            // console.log(event);
+            // console.log(slick);
+            // var i = (currentSlide ? currentSlide : 0);
+            // console.log(i);
+
+            var dataId = $('.slick-current').attr("data-slick-index");    
+            // console.log(dataId);
+            $('.flavor_label').removeClass("selected");
+            $('button.'+flavors[dataId]).addClass("selected");
+            
+        });
+
+    });
+
+    //scroll_effect
+    $(window).scroll(function () {
+        var scrollAnimationElm = document.querySelectorAll('.scroll');
+        var scrollAnimationFunc = function () {
+        for (var i = 0; i < scrollAnimationElm.length; i++) {
+            var triggerMargin = 300;
+            if (window.innerHeight > scrollAnimationElm[i].getBoundingClientRect().top + triggerMargin) {
+            scrollAnimationElm[i].classList.add('on');
+            }
+        }
+        }
+        window.addEventListener('load', scrollAnimationFunc);
+        window.addEventListener('scroll', scrollAnimationFunc);
     });
 
 
